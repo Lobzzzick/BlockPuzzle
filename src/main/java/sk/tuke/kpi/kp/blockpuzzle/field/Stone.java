8ba5f319097e27adb928446ad7ef567f0e890d65
Roman Rudenko <roman.rudenko@student.tuke.sk>
2025-03-19 23:12:23 +0100

    Upload New File

---
+++ src/main/java/sk/tuke/kpi/kp/blockpuzzle/field/Stone.java
@@ -0,0 +1,18 @@
+package sk.tuke.kpi.kp.blockpuzzle.field;
+
+
+public class Stone {
+    private int color;
+
+    public Stone(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
