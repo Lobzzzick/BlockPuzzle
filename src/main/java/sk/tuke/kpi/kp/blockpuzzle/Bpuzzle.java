70117e1e53f9c5d57c0a246bf60ea7909d1ac9fb
Roman Rudenko <roman.rudenko@student.tuke.sk>
2025-03-19 23:10:00 +0100

    Upload New File

---
+++ src/main/java/sk/tuke/kpi/kp/blockpuzzle/Bpuzzle.java
@@ -0,0 +1,10 @@
+package sk.tuke.kpi.kp.blockpuzzle;
+
+import sk.tuke.kpi.kp.blockpuzzle.game.ConsoleUI;
+
+public class Bpuzzle {
+    public static void main(String[] args) {
+        ConsoleUI ui = new ConsoleUI();
+        ui.start();
+    }
+}
